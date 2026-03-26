557552055dfbe47aeab93fcebed7975b179abd08
Abhaykumar <k.abhay@iitg.ac.in>
2026-03-26 19:14:15 +0530

    update api url

--- client/src/Header.js
+++ client/src/Header.js
@@ -6,7 +6,7 @@ import "./Header.css";
 export default function Header() {
   const { setUserInfo, userInfo } = useContext(UserContext);
   useEffect(() => {
-    fetch("http://localhost:4000/profile", {
+    fetch("https://blog-app-zptr.onrender.com/profile", {
       credentials: "include",
     }).then((response) => {
       response.json().then((userInfo) => {
@@ -16,7 +16,7 @@ export default function Header() {
   }, []);
 
   function logout() {
-    fetch("http://localhost:4000/logout", {
+    fetch("https://blog-app-zptr.onrender.com/logout", {
       credentials: "include",
       method: "POST",
     });
